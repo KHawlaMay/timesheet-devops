90c0dbb4183f5b55e6f26db9d1de12d4752e8b4a
khawla may <khaoula.may@esprit.tn>
2022-06-03 01:24:05 +0100

    commit en local git

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -22,6 +22,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 //modif pour git
 	//modif2 git
+	//modif 3 git
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
